d67370e617740e0be6dd03cb4e6f15a529c99b96
taeate <yes3144@nate.com>
2023-02-23 15:01:51 +0900

    상속을 통한 캐스팅 허용 방법 예제

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,34 +3,19 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        청둥오리 a청둥오리 = new 청둥오리();
-        a청둥오리.날다();
-
-        흰오리 a흰오리 = new 흰오리();
-        a흰오리.날다();
-
-        오리 a오리 = new 오리();
-        a오리.날다();
-
-        고무오리 a고무오리 = new 고무오리();
-        a고무오리.날다();
-        // 출력 : 오리가 날개로 날아갑니다.
+        무기 a무기 = new 칼();
+        a무기.공격();
+        // 출력 : 칼로 공격합니다.
     }
 }
-class 오리 {
-    void 날다(){
-        System.out.println("오리가 날개로 날아갑니다.");
+class 무기 {
+    void 공격(){}
+}
+class 칼 extends 무기 {
+    void 공격() {
+        System.out.println("칼로 공격합니다.");
     }
 }
-class 청둥오리 extends 오리 {
 
-}
-class 흰오리 extends 오리 {
-}
 
-class 고무오리 extends 오리 {
-    void 날다(){
-        System.out.println("고무오리인 저는 날수없어요 ㅠㅠ");
-    }
-}
 
