4718c07740f25bd02f76edc4c58f3e2d878963db
taeate <yes3144@nate.com>
2023-02-23 14:13:07 +0900

    class extends 수업

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,17 +2,35 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-//        System.out.println(new Solution().solution(2,3)); // -1
-//        System.out.println(new Solution().solution(11,11)); // 1
-//        System.out.println(new Solution().solution(7,99)); // -1
-    }
-}
-class Solution {
-    public int solution(int angle) {
-        if (angle == 180) return 4;
-        else if (angle > 90) return 3;
-        else if (angle < 90) return 1;
 
-        return angle;
+        청둥오리 a청둥오리 = new 청둥오리();
+        a청둥오리.날다();
+
+        흰오리 a흰오리 = new 흰오리();
+        a흰오리.날다();
+
+        오리 a오리 = new 오리();
+        a오리.날다();
+
+        고무오리 a고무오리 = new 고무오리();
+        a고무오리.날다();
+        // 출력 : 오리가 날개로 날아갑니다.
     }
 }
+class 오리 {
+    void 날다(){
+        System.out.println("오리가 날개로 날아갑니다.");
+    }
+}
+class 청둥오리 extends 오리 {
+
+}
+class 흰오리 extends 오리 {
+}
+
+class 고무오리 extends 오리 {
+    void 날다(){
+        System.out.println("고무오리인 저는 날수없어요 ㅠㅠ");
+    }
+}
+
