638673a9198809ff236e82492af52df5fc80c817
taeate <yes3144@nate.com>
2023-02-24 15:34:03 +0900

    생성자 사용1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,33 +3,49 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        전사 a전사 = new 전사();
-        a전사.공격();
-        // 출력 => 아직 무기가 없습니다.
-        a전사.a무기 = new 칼();
-        a전사.공격();
-        // 출력 => 칼로 공격합니다.
+        전사 a전사0 = new 전사();
+        a전사0.나이 = 20;
+        a전사0.이름 = "칸";
+        a전사0.성격 = "차가움";
+        a전사0.a무기 = new 활();
+
+        전사 a전사1 = new 전사();
+        전사 a전사2 = new 전사();
+        전사 a전사3 = new 전사();
+        전사 a전사4 = new 전사();
+        전사 a전사5 = new 전사();
+
+        System.out.println(a전사0.이름);
+        // 출력 : 칸
+        System.out.println(a전사1.이름);
+        // 출력 : No Name
+        System.out.println(a전사2.이름);
+        // 출력 : No Name
+        System.out.println(a전사3.이름);
+        // 출력 : No Name
+        System.out.println(a전사4.이름);
+        // 출력 : No Name
+        System.out.println(a전사5.이름);
+        // 출력 : No Name
     }
 }
-class 전사 {
-    무기 a무기;
-    public void 공격() {
-        if( a무기 == null) {
-            System.out.println("아직 무기가 없습니다.");
-        }else {
-            a무기.사용();
-        }
 
+class 전사 {
+    String 이름;
+    int 나이;
+    String 성격;
+    무기 a무기;
+    전사(){
+        이름 = "No Name";
     }
 }
 class 무기 {
-    public void 사용(){}
+
 }
-class 칼 extends 무기 {
-    public void 사용() {
-        System.out.println("칼로 공격합니다.");
-    }
+class 활 extends 무기 {
+
 }
+
 
 
 
