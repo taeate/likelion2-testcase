cb4561451f4004cda3485354e97a26ab13edaaf3
taeate <yes3144@nate.com>
2023-02-24 15:37:33 +0900

    생성자 사용2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,49 +3,21 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        전사 a전사0 = new 전사();
-        a전사0.나이 = 20;
-        a전사0.이름 = "칸";
-        a전사0.성격 = "차가움";
-        a전사0.a무기 = new 활();
+        사람 a사람 = new 사람();
+        a사람.나이 = 10;
+        a사람.a왼팔 = new 팔();
 
-        전사 a전사1 = new 전사();
-        전사 a전사2 = new 전사();
-        전사 a전사3 = new 전사();
-        전사 a전사4 = new 전사();
-        전사 a전사5 = new 전사();
-
-        System.out.println(a전사0.이름);
-        // 출력 : 칸
-        System.out.println(a전사1.이름);
-        // 출력 : No Name
-        System.out.println(a전사2.이름);
-        // 출력 : No Name
-        System.out.println(a전사3.이름);
-        // 출력 : No Name
-        System.out.println(a전사4.이름);
-        // 출력 : No Name
-        System.out.println(a전사5.이름);
-        // 출력 : No Name
+        System.out.println(a사람.a왼팔.길이 + "cm");
+        // 출력 : 100cm
     }
 }
-
-class 전사 {
-    String 이름;
+class 사람 {
     int 나이;
-    String 성격;
-    무기 a무기;
-    전사(){
-        이름 = "No Name";
-    }
+    팔 a왼팔;
 }
-class 무기 {
-
+class 팔 extends 사람{
+    int 길이 = 100;
 }
-class 활 extends 무기 {
-
-}
-
 
 
 
