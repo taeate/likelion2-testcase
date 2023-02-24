fc13bed4dc7203397c80f3f65f58d65e048b57d0
taeate <yes3144@nate.com>
2023-02-24 16:05:06 +0900

    생성자 3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,21 +3,37 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        사람 a사람 = new 사람();
-        a사람.나이 = 10;
-        a사람.a왼팔 = new 팔();
+        int 엄지손가락_길이 = new 사람().a왼팔.a손.a엄지손가락.길이;
 
-        System.out.println(a사람.a왼팔.길이 + "cm");
-        // 출력 : 100cm
+        System.out.println(엄지손가락_길이 + "cm");
+        // 출력 : 5cm
     }
 }
 class 사람 {
-    int 나이;
+    사람(){
+        a왼팔 = new 팔();
+    }
     팔 a왼팔;
 }
-class 팔 extends 사람{
-    int 길이 = 100;
+class 팔{
+    팔(){
+        a손 = new 핸드();
+    }
+    핸드 a손;
 }
+class 핸드 {
+    핸드(){
+        a엄지손가락 = new 손가락();
+    }
+    손가락 a엄지손가락;
+}
+class 손가락 {
+    int 길이;
+    손가락(){
+        this.길이 = 5;
+    }
+}
+
 
 
 
