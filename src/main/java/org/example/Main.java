3bc8d31944f72bfd11f95d8467d67f43400ffc7a
taeate <yes3144@nate.com>
2023-02-28 15:25:44 +0900

    오름차순 내림 차순 정렬

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,37 +1,31 @@
 package org.example;
 
-public class Main {
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+class Main {
     public static void main(String[] args) {
+        List<Integer> al = new ArrayList<>() {{
+            add(30);
+            add(10);
+            add(20);
+        }};
 
-        int 엄지손가락_길이 = new 사람().a왼팔.a손.a엄지손가락.길이;
+        Collections.sort(al, (e2, e1) -> {
+            // return e2 > e1 ? 1 : -1; // v1 오름차순 정렬
+            // return e2 > e1 ? -1 : 1; // v2 내림차순 정렬
+            // return e2 - e1; // v3 오름차순 정렬
+            return e1 - e2; // v4 내림차순 정렬
+        });
 
-        System.out.println(엄지손가락_길이 + "cm");
-        // 출력 : 5cm
+        Collections.sort(al, (e2, e1) -> e1 - e2);
+
+        System.out.println(al);
     }
 }
-class 사람 {
-    사람(){
-        a왼팔 = new 팔();
-    }
-    팔 a왼팔;
-}
-class 팔{
-    팔(){
-        a손 = new 핸드();
-    }
-    핸드 a손;
-}
-class 핸드 {
-    핸드(){
-        a엄지손가락 = new 손가락();
-        a엄지손가락.길이 = 5;
-    }
-    손가락 a엄지손가락;
-}
-class 손가락 {
-    int 길이;
-}
-
-
 
 
