055c84001f1d3d9e92c9ca0b3452176900df59ea
taeate <yes3144@nate.com>
2023-03-02 10:06:25 +0900

    명언 프로그램 종료 코드

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class App {
+    private final Scanner sc;
+
+    public App(Scanner sc) {
+        this.sc = sc;
+    }
+
+    public void run() {
+
+        System.out.println("===== 명언 앱 ====");
+
+        while (true) {
+
+            System.out.print("명령) ");
+
+            String cmd = sc.nextLine().trim();
+
+            if (cmd.equals("하하")) {
+                System.out.println("종료");
+                break;
+            }
+            else if (cmd.equals("등록")) {
+
+                String wiseSaying = sc.nextLine().trim();
+                String writer = sc.nextLine().trim();
+
+                System.out.println("번 명언이 등록되었습니다.");
+            }
+        }
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,25 +7,18 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+
 class Main {
     public static void main(String[] args) {
-        List<Integer> al = new ArrayList<>() {{
-            add(30);
-            add(10);
-            add(20);
-        }};
 
-        Collections.sort(al, (e2, e1) -> {
-            // return e2 > e1 ? 1 : -1; // v1 오름차순 정렬
-            // return e2 > e1 ? -1 : 1; // v2 내림차순 정렬
-            // return e2 - e1; // v3 오름차순 정렬
-            return e1 - e2; // v4 내림차순 정렬
-        });
+        Scanner sc = new Scanner(System.in);
 
-        Collections.sort(al, (e2, e1) -> e1 - e2);
+        new App(sc).run();
 
-        System.out.println(al);
+        sc.close();
     }
 }
+
+
 
 
